002ddb3f6e0c48a8da5c13657bfeb1ff0f4e7a8b
Marco Carmona <elbuki@users.noreply.github.com>
2023-07-06 16:57:41 -0600

    Initial Commit

---
+++ AutoSwitcher/AutoSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSwitcherApp.swift
+//  AutoSwitcher
+//
+//  Created by Marco Carmona on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
